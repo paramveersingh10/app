e9d2ea2bf4a547ab9e3bb1896c3dcd6219b80d23
paramveersingh10 <120078017+paramveersingh10@users.noreply.github.com>
2023-10-23 08:40:34 -0800

    first commit

---
+++ page.js
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import StudentInfo from './StudentInfo';
+export default function Page()
+    {
+        return (
+            <div>
+                <h1>CPRG 306: Web Development 2 - Assigments </h1>
+                <StudentInfo/>
+                <Link href="\week2">week2</Link>
+                <p></p>
+                <Link href="\week3">week3</Link>
+                <p></p>
+                <Link href="\week4">week4</Link>
+                <p></p>
+                <Link href="\week5">week5</Link>
+                <p></p>
+                <Link href="\week6">week6</Link>
+            </div>  
+        )
+    }     
---
+++ week2/StudentInfo.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+export default function StudentInfo()
+ {
+    return (
+        <div>
+            <h1>Paramveer Singh</h1>
+            <p>CPRG 306 A</p>
+            <Link href="https://github.com/paramveersingh10/C--web">github link</Link>
+         </div>
+    )
+}
